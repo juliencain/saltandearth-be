1b2c10f9554faf98e3b7003003707e661785ba8c
Julien Smith <juliencain1933@gmail.com>
2025-03-03 21:42:20 -0600

    created and finished Restaurant View Page

--- saltandearthbe/urls.py
+++ saltandearthbe/urls.py
@@ -2,12 +2,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from saltandearthapp.views import CityView, UserView, LodgingView
+from saltandearthapp.views import CityView, UserView, LodgingView, RestaurantView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'cities', CityView, 'city')
 router.register(r'users', UserView, 'user')
 router.register(r'lodging', LodgingView, 'lodging')
+router.register(r'restaurants', RestaurantView, 'restaurant')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
